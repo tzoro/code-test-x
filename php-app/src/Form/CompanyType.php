5d4acff33f97c5ee81348cc99e7c9c5f6236711a
Tino Zorotovic <tino_zorotovic@epam.com>
2023-01-16 11:40:46 +0200

    JQueryUI date input

--- php-app/src/Form/CompanyType.php
+++ php-app/src/Form/CompanyType.php
@@ -12,14 +12,17 @@ class CompanyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $dateWidgetConfig = [
+            'widget' => 'single_text',
+            'html5' => false,
+            'attr' => ['class' => 'js-datepicker'],
+            'format'=>'yyyy-mm-dd'
+        ];
+
         $builder
             ->add('CompanySymbol')
-            ->add('StartDate', DateType::class, [
-
-            ])
-            ->add('EndDate', DateType::class, [
-
-            ])
+            ->add('StartDate', DateType::class, $dateWidgetConfig)
+            ->add('EndDate', DateType::class, $dateWidgetConfig)
             ->add('email')
         ;
     }
