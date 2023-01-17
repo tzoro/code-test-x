058bc39ab874636b324eab5289b725f49112a9d5
Tino Zorotovic <tino_zorotovic@epam.com>
2023-01-17 11:09:23 +0200

    Disable history on date input element

--- php-app/src/Form/CompanyType.php
+++ php-app/src/Form/CompanyType.php
@@ -15,7 +15,7 @@ class CompanyType extends AbstractType
         $dateWidgetConfig = [
             'widget' => 'single_text',
             'html5' => false,
-            'attr' => ['class' => 'js-datepicker'],
+            'attr' => ['class' => 'js-datepicker', 'autocomplete' => 'off'],
             'format'=>'yyyy-mm-dd'
         ];
 
