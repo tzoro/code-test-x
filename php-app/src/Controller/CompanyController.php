d2d26cf913a77d90a49bc292a7997703e6a92b5d
Tino Zorotovic <tino_zorotovic@epam.com>
2023-01-17 12:21:56 +0200

    Remove Company CRUD leftovers & DB dependency

--- php-app/src/Controller/CompanyController.php
+++ php-app/src/Controller/CompanyController.php
@@ -3,10 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Company;
-use App\Form\CompanyType;
 use App\Service\CurlGet;
+use App\Form\CompanyType;
 use App\Service\MailSender;
-use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,23 +14,14 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/company')]
 class CompanyController extends AbstractController
 {
-    #[Route('/', name: 'app_company_index', methods: ['GET'])]
-    public function index(CompanyRepository $companyRepository): Response
-    {
-        return $this->render('company/index.html.twig', [
-            'companies' => $companyRepository->findAll(),
-        ]);
-    }
-
     #[Route('/new', name: 'app_company_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, CompanyRepository $companyRepository, MailSender $mailSender): Response
+    public function new(Request $request, MailSender $mailSender, CurlGet $curlGet): Response
     {
         $company = new Company();
         $form = $this->createForm(CompanyType::class, $company);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $companyRepository->save($company, true);
 
             $mailSender->send(
                 $company->getEmail(),
@@ -39,55 +29,20 @@ class CompanyController extends AbstractController
                 'From ' . $company->getStartDate()->format('Y-m-d') . ' to ' . $company->getEndDate()->format('Y-m-d')
             );
 
-            return $this->redirectToRoute('app_company_index', [], Response::HTTP_SEE_OTHER);
+            $prices = $this->getHistoricalPrices($curlGet);
+            $graphData = $this->getGraphData($prices);
+
+            return $this->render('company/show.html.twig', [
+                'prices' => $prices,
+                'openData' => $graphData->open,
+                'closeData' => $graphData->close
+            ]);
         }
 
         return $this->renderForm('company/new.html.twig', [
             'company' => $company,
             'form' => $form,
         ]);
-    }
-
-    #[Route('/{id}', name: 'app_company_show', methods: ['GET'])]
-    public function show(Company $company, CurlGet $curlGet): Response
-    {
-        $prices = $this->getHistoricalPrices($curlGet);
-        $graphData = $this->getGraphData($prices);
-
-        return $this->render('company/show.html.twig', [
-            'company' => $company,
-            'prices' => $prices,
-            'openData' => $graphData->open,
-            'closeData' => $graphData->close
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_company_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Company $company, CompanyRepository $companyRepository): Response
-    {
-        $form = $this->createForm(CompanyType::class, $company);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $companyRepository->save($company, true);
-
-            return $this->redirectToRoute('app_company_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('company/edit.html.twig', [
-            'company' => $company,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_company_delete', methods: ['POST'])]
-    public function delete(Request $request, Company $company, CompanyRepository $companyRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$company->getId(), $request->request->get('_token'))) {
-            $companyRepository->remove($company, true);
-        }
-
-        return $this->redirectToRoute('app_company_index', [], Response::HTTP_SEE_OTHER);
     }
 
     private function getGraphData(Array $prices): \stdClass
